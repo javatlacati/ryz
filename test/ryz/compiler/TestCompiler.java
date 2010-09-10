751f0c758efd5fc1bd0bb9db0c73005ba95ad23b
Oscar Reyes <OscarRyz@users.noreply.github.com>
2010-09-10 16:19:26 -0500

    changed test/ryz/compiler/TestCompiler.java
    Added the ability to load dinamically the source files to be tested
    and create a separate test for each one.
    
    There's a "test-specification" to  follow, described in
    
    test-resources/readme.txt
    
    .

--- test/ryz/compiler/TestCompiler.java
+++ test/ryz/compiler/TestCompiler.java
@@ -2,6 +2,7 @@ package ryz.compiler;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -21,8 +22,6 @@ import java.util.Properties;
  * and it will compile them and assert they match the specification
  * described by the properties object loaded from that same file.
  *
- * TODO: Use the data provider from testng
- *
  * User: oscarryz
  * Date: Sep 8, 2010
  * Time: 8:21:57 PM
@@ -38,55 +37,67 @@ public class TestCompiler {
                                        new File("./test-resources/output/"));
     }
 
-    public void runTests() throws IOException, ClassNotFoundException {
+    @AfterMethod
+    void cleanUp() {
+        testUtil.deleteFromOutput("load/test/First.class");
+    }
+    
 
-        // collect all source files
-        List<File> sourceFiles = collectSourceFiles();
-        Properties testProperties = null;
 
-        // and for each one of them
-        for (File testFile : sourceFiles)
-            try {
 
-                // assert the properties match
-                testProperties = getPropertiesFrom(testFile);
-                String className = testProperties.getProperty("className");
-                // if they have a "className" property
-                if( className == null ){
-                    continue; // we won't be able to test this one
-                }
+    @Test(dataProvider="sourceFiles")
+    public void runTests(Properties spec) throws IOException, ClassNotFoundException {
 
-                // TODO: move out this, when each file represent a single test case
-                testUtil = TestUtil.createWith(new File("./test-resources/00.loading/"),
-                                               new File("./test-resources/output/"));
+        String className = spec.getProperty("className");
+        String testFile = spec.getProperty("fileName");
 
-                testUtil.assertMissing(className);
-                testUtil.compile(testFile.getName());
-                Class clazz = testUtil.assertExists(
-                                       testProperties.getProperty("className"));
+        try {
 
-                assertAttributes(testProperties, testFile, clazz);
-                assertMethods(testProperties, testFile, clazz);
+            testUtil.assertMissing(className);
+            testUtil.compile(testFile);
+            Class clazz = testUtil.assertExists(className);
 
-            } finally {
-                    testUtil.deleteFromOutput(testProperties.getProperty("classFile"));
-            }
+            assertAttributes(spec, testFile, clazz);
+            assertMethods(spec, testFile, clazz);
+
+        } finally {
+            testUtil.deleteFromOutput(className);
+        }
 
     }
 
 
+        @DataProvider(name="sourceFiles")
+    private Object [][] loadSourceFiles() throws IOException {
+
+        // collect all source files
+        List<File> sourceFiles = collectSourceFiles();
+        List<Properties[]> sourceProperties = new ArrayList<Properties[]>();
+
+        for (File testFile : sourceFiles) {
+            // assert the properties match
+            sourceProperties.add(new Properties[]{
+                    getPropertiesFrom(testFile)
+            });
+        }
+        return  sourceProperties.toArray(
+                    new Properties[sourceProperties.size()][]
+                );
+    }
+
+
     /**
-     * Assert that the given class has the attributes defined in the testProperties
+     * Assert that the given class has the attributes defined in the spec
      * object.
-     * @param testProperties
-     * @param testFile
+     * @param spec
+     * @param file
      * @param clazz
      */
-    private void assertAttributes(Properties testProperties, File testFile, Class clazz) {
-        if( clazz == null || testProperties.getProperty("attributes") == null ){
+    private void assertAttributes(Properties spec, String file, Class clazz) {
+        if( clazz == null || spec.getProperty("attributes") == null ){
             return ;
         }
-        String attributes = testProperties.getProperty("attributes");
+        String attributes = spec.getProperty("attributes");
         for (String pairs : attributes.split(",")) {
             String[] nameType = pairs.split(":");
             boolean matched = false;
@@ -97,32 +108,34 @@ public class TestCompiler {
                     break;
                 }
             }
-            assert matched : testFile.getName() + " didn't fulfilled: " + pairs + "attribute";
+            assert matched : file + " didn't fulfilled: " + pairs + "attribute";
         }
     }
 
     /**
-     * Assert the given class has all the methods defined in the testProperties
-     * @param testProperties
-     * @param testFile
+     * Assert the given class has all the methods defined in the spec
+     * @param spec
+     * @param file
      * @param clazz
      */
-    private void assertMethods(Properties testProperties, File testFile, Class clazz) {
-        if( clazz == null || testProperties.getProperty("methods") == null ){
+    private void assertMethods(Properties spec, String file, Class clazz) {
+        if( clazz == null || spec.getProperty("methods") == null ){
             return; 
         }
-        String methods = testProperties.getProperty("methods");
+        String methods = spec.getProperty("methods");
         for (String pairs : methods.split(",")) {
             String[] nameType = pairs.split(":");
             boolean matched = false;
             for (Method m : clazz.getDeclaredMethods()) {
                 if (m.getName().equals(nameType[0].trim())
-                        && m.getReturnType().toString().equals(nameType[1].trim())) {
+                        && m.getReturnType()
+                            .toString()
+                            .equals(nameType[1].trim())) {
                     matched = true;
                     break;
                 }
             }
-            assert matched : testFile.getName() + " didn't fulfilled: " + pairs + "attribute";
+            assert matched : file + " didn't fulfilled: " + pairs + "attribute";
         }
     }
 
@@ -143,6 +156,7 @@ public class TestCompiler {
                 inStream.close();
             } catch (IOException ioe ){}
         }
+        p.setProperty("fileName", testFile.getName());
         return p;
 
     }
@@ -159,16 +173,11 @@ public class TestCompiler {
         for( File file : testFiles) if( file.isDirectory() ) {
             files.addAll( Arrays.asList(file.listFiles(new FileFilter(){
                 public boolean accept(File pathname) {
-                    return pathname.getName().endsWith(".ryz");
+                    return pathname.getName().endsWith("Spec.ryz");
                 }
             })));
         }
         return files;
     }
 
-
-    @AfterMethod
-    void cleanUp() {
-        testUtil.deleteFromOutput("load/test/First.class");
-    }
 }
